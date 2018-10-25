9ecb5632306c18da0891e8d6c1010584f1e25d0e
Mario Arranz <marioarranzr@gmail.com>
2018-10-25 15:25:24 +0100

    router and index reorganization

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = (props) => {
+  return (
+    <header className="top">
+        <h1>
+          NotFound
+        </h1>
+    </header>
+)
+}
+
+export default NotFound;
